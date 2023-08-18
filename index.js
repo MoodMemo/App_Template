b861653b77ba33613e5cb7a29f8dba8d07a9791a
juLeena <daitos2002@naver.com>
2023-08-18 13:57:33 +0900

    fix : font size fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text, TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {Amplify} from 'aws-amplify';
@@ -54,6 +54,12 @@ Sentry.init({
 
 AmplitudeInit();
 amplitude.beginSession(); // 앱 시작
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
 
 AppRegistry.registerComponent(appName, () => App);
 
