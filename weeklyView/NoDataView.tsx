b9edecdaee066289598b21c87e4598f84eb41997
12novel30 <12novel30@naver.com>
2023-10-19 12:04:55 +0900

    feat: HH-1315 위클리뷰 디자인 통일

--- weeklyView/NoDataView.tsx
+++ weeklyView/NoDataView.tsx
@@ -27,6 +27,7 @@ dayjs.extend(timezone);
 import {default as Text} from "../CustomText"
 
 import * as Sentry from '@sentry/react-native';
+import { styles } from 'react-native-gifted-charts/src/LineChart/styles';
 
 
 /** no navigate */
@@ -34,12 +35,17 @@ export const FromFutureView = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', }}>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', }}>
+        <View>
+          <View style={[bubbleStyles.container, {width: 180, backgroundColor: '#dbdbdb', zIndex: 100}]}>
+            <Text style={{fontSize: 16, color: '#fff', }}>미래에서 왔냐무?</Text>
+            <Text style={{fontSize: 16, color: '#fff', }}>타임머신은 없는데무!</Text>
+          </View>
+          <View style={[bubbleStyles.tail, {backgroundColor: '#dbdbdb'}]}></View>
+        </View>
         <Image 
           source={require('../assets/boring_gray_0904.png')}
-          style={{ width: 124, height: (113 * 124) / 124 , marginBottom: 16}} // 비율을 유지하며 height 자동 조절
+          style={{ width: 124, height: (113 * 124) / 124 , marginTop: 20}} // 비율을 유지하며 height 자동 조절
         />
-        <Text style={{fontSize: 16, color: '#dbdbdb', }}>미래에서 왔냐무?</Text>
-        <Text style={{fontSize: 16, color: '#dbdbdb', }}>타임머신은 없는데무!</Text>
       </View>
 
     </View>
@@ -49,11 +55,16 @@ export const MooWasBoredView = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', }}>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', }}>
+      <View>
+          <View style={[bubbleStyles.container, {width: 130, backgroundColor: '#dbdbdb'}]}>
+            <Text style={{fontSize: 16, color: '#fff',}}>심심했다무...</Text>
+          </View>
+          <View style={[bubbleStyles.tail, {backgroundColor: '#dbdbdb', }]}></View>
+        </View>
         <Image 
           source={require('../assets/boring_gray_0904.png')}
-          style={{ width: 124, height: (113 * 124) / 124 , marginBottom: 16}} // 비율을 유지하며 height 자동 조절
+          style={{ width: 124, height: (113 * 124) / 124 , marginTop: 20}} // 비율을 유지하며 height 자동 조절
         />
-        <Text style={{fontSize: 16, color: '#dbdbdb', }}>심심했다무...</Text>
       </View>
 
     </View>
@@ -70,16 +81,22 @@ export const TellMeYourDayView = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', }}>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', }}>
+        <View>
+          <View style={[bubbleStyles.container, {backgroundColor: '#dbdbdb', zIndex: 100}]}>
+            <Text style={{fontSize: 16, color: '#fff', }}>심심하다무...</Text>
+            <Text style={{fontSize: 16, color: '#fff', }}>무슨 일이 있었는지 들려달라무!</Text>
+          </View>
+          <View style={[bubbleStyles.tail, {backgroundColor: '#dbdbdb'}]}></View>
+        </View>
+
         <Image 
           source={require('../assets/boring_gray_0904.png')}
-          style={{ width: 124, height: (113 * 124) / 124 , marginBottom: 16}} // 비율을 유지하며 height 자동 조절
+          style={{ width: 124, height: (113 * 124) / 124 , marginTop: 20, marginBottom: 40}} // 비율을 유지하며 height 자동 조절
         />
-        <Text style={{fontSize: 16, color: '#dbdbdb', }}>심심하다무...</Text>
-        <Text style={{fontSize: 16, color: '#dbdbdb', }}>무슨 일이 있었는지 들려달라무!</Text>
 
-        <TouchableOpacity style={typeChangeBtnStyles.nudgingBtn} 
+        <TouchableOpacity style={[bubbleStyles.reply, {width: 184, height: 46, marginBottom: 10}]} 
           onPress={() => {handleRecordEmotion(); amplitude.pushStampInTellMeYourDayView();}}>
-          <Text style={{fontSize: 16, color: '#ffffff', fontWeight: '600'}}>감정 스탬프 기록하기</Text>
+          <Text style={{fontSize: 16, color: '#72D193', fontWeight: '600'}}>감정 스탬프 기록하기</Text>
         </TouchableOpacity>
       </View>
 
@@ -96,19 +113,22 @@ export const PleaseOneMoreStampView = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', }}>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', }}>
-        <Image 
-          source={require('../assets/emptyMoo.png')}
-          style={{ width: 104, height: (107 * 104) / 104 , marginBottom: 16}} // 비율을 유지하며 height 자동 조절
-        />
-        <View style={{ alignItems: 'center', flexDirection: 'row', marginTop: 10, }}>
-          <Text style={{fontSize: 16, color: '#495057', }}>스탬프 </Text>
-          <Text style={{fontSize: 16, color: '#495057', fontWeight: '700', marginBottom:(Platform.OS==='android' ? 3 : 0)}}>한 개만 더</Text>
-          <Text style={{fontSize: 16, color: '#495057', }}> 찍으면, 일기를 만들 수 있다무!</Text>
+        
+        <View>
+          <View style={[bubbleStyles.container, {backgroundColor: '#dbdbdb', zIndex: 100}]}>
+            <Text style={{fontSize: 16, color: '#fff', }}>스탬프 한 개만 더 찍고 오라무!</Text>
+          </View>
+          <View style={[bubbleStyles.tail, {backgroundColor: '#dbdbdb'}]}></View>
         </View>
 
-        <TouchableOpacity style={[typeChangeBtnStyles.nudgingBtn,{width:200}]} 
+        <Image 
+          source={require('../assets/emptyMoo.png')}
+          style={{ width: 104, height: (107 * 104) / 104 , marginTop: 20, marginBottom: 40}} // 비율을 유지하며 height 자동 조절
+        />
+
+        <TouchableOpacity style={[bubbleStyles.reply, {width: 184, height: 46, marginBottom: 10}]} 
           onPress={() => {handleRecordEmotion(); amplitude.pushStampInPleaseOneMoreStampView();}}>
-          <Text style={{fontSize: 16, color: '#ffffff', fontWeight: '600'}}>감정 스탬프 기록하기</Text>
+          <Text style={{fontSize: 16, color: '#72D193', fontWeight: '600'}}>하나 더 누르고 올게!</Text>
         </TouchableOpacity>
       </View>
 
@@ -125,19 +145,28 @@ export const PleaseOneMoreStampMini = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', }}>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', }}>
-        <Image 
-          source={require('../assets/emptyMoo.png')}
-          style={{ width: 104, height: (107 * 104) / 104 , marginBottom: 16}} // 비율을 유지하며 height 자동 조절
-        />
-        <View style={{ alignItems: 'center', flexDirection: 'row', marginTop: 10, }}>
-          <Text style={{fontSize: 16, color: '#495057', }}>스탬프 </Text>
-          <Text style={{fontSize: 16, color: '#495057', fontWeight: '700', marginBottom:(Platform.OS==='android' ? 3 : 0)}}>한 개만 더</Text>
-          <Text style={{fontSize: 16, color: '#495057', }}> 찍으면, 일기를 만들 수 있다무!</Text>
+
+
+        <View>
+          <View style={[bubbleStyles.container, {zIndex: 100, width: 180}]}>
+            <View style={{ alignItems: 'center', flexDirection: 'row', }}>
+              <Text style={{fontSize: 14, color: '#fff', }}>스탬프 </Text>
+              <Text style={{fontSize: 14, color: '#fff', fontWeight: '700', marginBottom:(Platform.OS==='android' ? 3 : 0)}}>한 개만 더</Text>
+              <Text style={{fontSize: 14, color: '#fff', }}> 누르면,</Text>
+            </View>
+            <Text style={{fontSize: 14, color: '#fff', }}>일기를 만들 수 있다무!</Text>
+          </View>
+          <View style={[bubbleStyles.tail, {}]}></View>
         </View>
 
-        <TouchableOpacity style={[typeChangeBtnStyles.nudgingBtn,{width:200}]} 
+        <Image 
+            source={require('../assets/colorMooMedium.png')}
+            style={{ width: 104, height: (110 * 104) / 104 , marginTop: 20, marginBottom:20}} // 비율을 유지하며 height 자동 조절
+          />
+
+        <TouchableOpacity style={[bubbleStyles.reply, {width: 184, height: 35, marginBottom: 10, padding: 5}]} 
           onPress={() => {handleRecordEmotion(); amplitude.pushStampInPleaseOneMoreStampViewInStampSwitch();}}>
-          <Text style={{fontSize: 16, color: '#ffffff', fontWeight: '600'}}>감정 스탬프 기록하기</Text>
+          <Text style={{fontSize: 14, color: '#72D193', fontWeight: '600'}}>하나 더 누를래!</Text>
         </TouchableOpacity>
       </View>
 
@@ -193,6 +222,48 @@ const typeChangeBtnStyles = StyleSheet.create({
     justifyContent: 'center',
   },
   deactiveFont: {fontSize: 14, color: '#B7B7B7', fontWeight:'400'},
+});
+const bubbleStyles = StyleSheet.create({
+  container: {
+    backgroundColor: '#72D193',
+    padding: 10,
+    // maxWidth: 200,
+    width: 220,
+    alignSelf: 'flex-start', // 좌측 정렬로 변경
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    // borderBottomLeftRadius: 0, // 우측 하단을 둥글게
+    position: 'relative',
+    overflow: 'hidden', // 클리핑 적용
+  },
+  tail: {
+    position: 'absolute',
+    width: 20, // 꼬리의 길이
+    height: 20, // 꼬리의 높이
+    left: 10, // 꼬리 위치
+    bottom: -5, // 꼬리 위치
+    backgroundColor: '#72D193',
+    transform: [{ rotate: '45deg' }],
+    borderTopLeftRadius: 10, // 둥글게 만들기
+    // borderBottomLeftRadius: 10,
+    // borderTopRightRadius: 10
+  },
+  reply: {
+    backgroundColor: '#fff',
+    padding: 7,
+    // maxWidth: 200,
+    width: 200,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    // borderBottomLeftRadius: 0, // 우측 하단을 둥글게
+    position: 'relative',
+    borderColor: '#72D193',
+    borderWidth: 1,
+    overflow: 'hidden', // 클리핑 적용
+  },
 });
 
 
