252047d7fdd926ea09886bb76110a4cc71f993eb
정세희 <zanne1218@naver.com>
2023-11-02 23:56:23 +0900

    Feat: Weekly.tsx - Timeline, DailyReport 사진
    Realm에 저장된 item.imageUrl 값을 통해 뷰 띄우기

--- weeklyView/Weekly.tsx
+++ weeklyView/Weekly.tsx
@@ -738,7 +738,21 @@ const Weekly = () => {
                         <View style={Timelinestyles.line}></View>
                         <Text style={Timelinestyles.title}>{item.memo}</Text>
                         {/* <Text style={styles.title}>{item.imageUrl}</Text> */}
-
+                        {item.imageUrl && 
+                          <Image 
+                            source={{ uri: item.imageUrl }} 
+                            style={{ width: 54, height: 54, borderRadius: 4, marginHorizontal: 10, marginBottom: 10 }}  // adjust width and height as needed
+                            onLoadEnd={() => console.log(item.imageUrl)}
+                          />
+                        }
+                        {/* // 이미지 여러개일 경우 */}
+                        {/* {item.imageUrl && item.imageUrl.map((url) => (
+                          <Image 
+                            source={{ uri: url }} 
+                            style={{ width: 54, height: 54, borderRadius: 4, marginHorizontal: 10, marginBottom: 10 }}  // adjust width and height as needed
+                          />
+                        ))} */}
+  
                       </View>
                     </View>
                   ))}
@@ -854,6 +868,21 @@ const Weekly = () => {
                     ))}
                   </View>
                 )}
+                {/* 해당 날짜에 timelineData에 기록된 모든 사진들 가로로 */}
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{ marginTop: 10, }}>
+                {timelineData.map((item, index) => (
+                  <View key={index}
+                    style={{ marginLeft: index === 0 ? 0 : 12, }}
+                  >
+                    {item.imageUrl && 
+                      <Image 
+                        source={{ uri: item.imageUrl }} 
+                        style={{ width: 80, height: 80, borderRadius: 6, marginBottom: 10 }}  // adjust width and height as needed
+                      />
+                    }
+                  </View>
+                ))}
+                </ScrollView>
               </View>
             </View>
           </ScrollView>
